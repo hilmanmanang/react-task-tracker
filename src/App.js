9b5af368c17f7b8d6d2f2bdd272ee1553b8aef0f
Muhammad Hilman Bin Ahmad <hilman.ahmad@tmrnd.com.my>
2021-04-21 17:20:06 +0800

    commit this

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Haluuuuu.. Selamat Berbuka bro!</p>
+        <p>Haluuuuu.. Selamat Berbuka bro!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
