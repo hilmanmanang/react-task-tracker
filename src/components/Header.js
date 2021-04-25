767ca0499251fc1c463ad2f1e425dacbeae5d0bb
Muhammad Hilman Bin Ahmad <hilman.ahmad@tmrnd.com.my>
2021-04-26 04:47:56 +0800

    ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import Button from './Button'
 
 const Header = () => {
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
         <header className='header'>
             <h1>Task Tracker</h1> 
-            <Button color='green' text='Hello'/>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
