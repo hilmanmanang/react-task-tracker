6787226b49f871b1ec290f23e08549aa45ec4755
Muhammad Hilman Bin Ahmad <hilman.ahmad@tmrnd.com.my>
2021-04-27 17:37:53 +0800

    complete

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import  { Link } from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+           <Link to='/'>Back</Link>
+           <h4>Created by Hilman Ahmad</h4> 
+        </div>
+    )
+}
+
+export default About
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
-const Header = () => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
-            <h1>Task Tracker</h1> 
-            <Button color='green' text='Add' onClick={onClick}/>
+            <h1>{title}</h1> 
+            {location.pathname === '/' && <Button
+                color={showAdd ? 'red' : 'green'} 
+                text={showAdd ? 'Close' : 'Add'} 
+                onClick={onAdd}
+            />}
         </header>
     )
 }
